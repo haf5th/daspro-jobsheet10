b8a968bd241dea1dfb141405c550644e30094116
haf5th <hafiflearning@gmail.com>
2024-11-07 08:50:03 +0700

    last commit

--- Bioskop11.java
+++ Bioskop11.java
@@ -9,10 +9,29 @@ public class Bioskop11 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
         
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        System.out.println(penonton.length);
+        
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        System.out.println("penonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(",",penonton[i]));
+        }
+        //for (int i = 0; i < penonton.length; i++) {
+          //  System.out.println("panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        //}
+        //System.out.println(penonton.length);
+        //System.out.println(penonton[0].length);
+        //System.out.println(penonton[1].length);
+        //System.out.println(penonton[2].length);
+        //System.out.println(penonton[3].length);
+        //System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        //System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        //System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        //System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
     }
 }
